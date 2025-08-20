6ab17aa9138cee7c80130d3f95f7dbfcaf34fb03
Alexander <a.v.chervyakov03@gmail.com>
2025-08-21 00:48:57 +0300

    hw1

---
+++ hw1.py
@@ -0,0 +1,23 @@
+s1 = input()
+s2 = input()
+s3 = input()
+print(s1 + s2 + s3)
+
+
+a = int(input())
+b = int(input())
+print(2**8 * a**8 - 2**4 * a**4 + 2**2 * a**2 - 2*b + 1/2 * b**(1/2) + a * b**(b+a) / 2)
+
+
+print("23!")
+
+
+print("(123 + 456 - 789) % 10 = 0")
+
+
+a = int(input())
+print(a + 1, a - 1, sep='')
+
+
+a = int(input())
+print(a//100//1000)
